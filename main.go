15fbcb33cfc74b79f23e0262a2984f9b79d54e81
Sn0wo <152751263+Sn0wo2@users.noreply.github.com>
2024-04-26 07:09:41 +0800

    Add http request curl support
    看注释

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 // 360路由器限制，写了个HostFilter，应该兼容360路由器基本型号的后台Host限制
 // 不兼容自己改
 // chat.openai.com
+// 使用请将浏览器F12手动设置一次Host观察请求，复制为CURL CMD 将请求Body的Host替换为书输出内容，注意，不要替换了不该替换的符号
 func main() {
 	currentDir, err := os.Getwd()
 	if err != nil {
@@ -33,6 +34,7 @@ func main() {
 	defer outputFile.Close()
 	regex := regexp.MustCompile(`^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+([\w\.-]+)`)
 	scanner := bufio.NewScanner(hostsFile)
+	var outputLines []string
 	for scanner.Scan() {
 		line := scanner.Text()
 		matches := regex.FindStringSubmatch(line)
@@ -40,16 +42,19 @@ func main() {
 			domain := matches[2]
 			segments := strings.Split(domain, ".")
 			if len(segments) <= 5 && len(domain) <= 63 {
-				_, err := outputFile.WriteString(matches[1] + " " + domain + "\n")
-				if err != nil {
-					fmt.Println("写入输出文件时出错:", err)
-					return
-				}
+				outputLine := matches[1] + "^%^20" + domain
+				outputLines = append(outputLines, outputLine)
 			}
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Println("读取hosts文件时出错:", err)
+		return
+	}
+	outputContent := strings.Join(outputLines, "^%^0A")
+	_, err = outputFile.WriteString(outputContent)
+	if err != nil {
+		fmt.Println("写入输出文件时出错:", err)
 		return
 	}
 	fmt.Println("筛选完成，结果已写入host.txt文件。")
